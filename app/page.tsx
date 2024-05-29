f021e88c01699b565e109c8554dae542f055fffe
Sion Kim <94289633+mandlake@users.noreply.github.com>
2024-05-29 16:28:46 +0900

    240529 add :root

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import Login from "./pages/login/page";
 
 function Home() {
   const router = useRouter();
@@ -9,22 +8,22 @@ function Home() {
     <>
       <div className="flex flex-col justify-center items-center w-screen h-screen">
         <div className="flex flex-col gap-[0.5vh]">
-          <div className="text-[#E0E7E9] bg-[#354649] flex flex-col justify-center items-center align-middle w-[41.53vw] h-[18.85vh]">
+          <div className="text-[var(--color-Harbor-firth)] bg-[var(--color-Harbor-first)] flex flex-col justify-center items-center align-middle w-[41.53vw] h-[18.85vh]">
             <h1 className="font-normal text-[9.77vh] my-[4.54vh]">Lawmate</h1>
           </div>
           <div className=" justify-start flex flex-col gap-[0.5vh]">
-            <div className="bg-[#E0E7E9] w-[20vw] hover:bg-gradient-to-r from-[#354649] to-[#E0E7E9] transition duration-500 ease-in-out px-[2vw]">
+            <div className="bg-[var(--color-Harbor-firth)] w-[20vw] hover:bg-gradient-to-r from-[var(--color-Harbor-first)] to-[var(--color-Harbor-firth)] transition duration-500 ease-in-out px-[2vw]">
               <button
                 onClick={() => router.push(`/pages/join`)}
-                className="text-[#6C7A89] hover:text-[#E0E7E9] transition duration-500 ease-in-out"
+                className="text-[var(--color-Harbor-second)] hover:text-[var(--color-Harbor-firth)] transition duration-500 ease-in-out"
               >
                 회원가입으로 이동 -&gt;
               </button>
             </div>
-            <div className="bg-[#E0E7E9] w-[20vw] hover:bg-gradient-to-r from-[#354649] to-[#E0E7E9] transition duration-500 ease-in-out px-[2vw]">
+            <div className="bg-[var(--color-Harbor-firth)] w-[20vw] hover:bg-gradient-to-r from-[var(--color-Harbor-first)] to-[var(--color-Harbor-firth)] transition duration-500 ease-in-out px-[2vw]">
               <button
                 onClick={() => router.push(`/pages/login`)}
-                className="text-[#6C7A89] hover:text-[#E0E7E9] transition duration-500 ease-in-out"
+                className="text-[var(--color-Harbor-second)] hover:text-[var(--color-Harbor-firth)] transition duration-500 ease-in-out"
               >
                 로그인으로 이동 -&gt;
               </button>
